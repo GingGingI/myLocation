b2738192e8a087d8031dffaac42e6fb655963679
GingGingI <sma0147@naver.com>
2019-06-17 19:12:36 +0900

    locations

--- app/src/main/java/c/gingdev/mylocation/MainActivity.kt
+++ app/src/main/java/c/gingdev/mylocation/MainActivity.kt
@@ -11,12 +11,12 @@ import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import java.util.*
-import com.google.android.libraries.places.internal.i
-import android.R.attr.data
 import android.util.Log
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.widget.AutocompleteActivity
-
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.dialog_location.*
 
 
 class MainActivity : AppCompatActivity(),
@@ -24,13 +24,20 @@ class MainActivity : AppCompatActivity(),
     GoogleMap.OnMapClickListener {
     private val AUTOCOMPLETE_REQUEST_CODE = 1
 
+//    Map
     private var map: GoogleMap? = null
     private var fromLatLng: LatLng? = null
     private var toLatLng: LatLng? = null
 
+    private var switch: Int = -1
+
+//    BottomSheet
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
+//    AutoComplete
     private val placeIntente by lazy {
-        Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,
-            Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG)).build(this) }
+        Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN,
+            Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG)).build(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,8 +49,6 @@ class MainActivity : AppCompatActivity(),
         val map
                 = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         map?.getMapAsync(this)
-
-        startActivityForResult(placeIntente, AUTOCOMPLETE_REQUEST_CODE)
     }
 
 //    Map
@@ -51,6 +56,15 @@ class MainActivity : AppCompatActivity(),
     override fun onMapReady(map: GoogleMap?) {
         map?.setOnMapClickListener(this)
         this.map = map
+        bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottomSheet))
+        fromBtn.setOnClickListener {
+            switch = 0
+            startActivityForResult(placeIntente, AUTOCOMPLETE_REQUEST_CODE)
+        }
+        toBtn.setOnClickListener {
+            switch = 1
+            startActivityForResult(placeIntente, AUTOCOMPLETE_REQUEST_CODE)
+        }
     }
 
     override fun onMapClick(latlng: LatLng?) {
@@ -78,12 +92,21 @@ class MainActivity : AppCompatActivity(),
 //    AutoComplete
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
         if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
                 val place = Autocomplete.getPlaceFromIntent(data!!)
                 Log.i("AutoComplete", "Place: ${place.name} , ${place.id} , ${place.latLng?.latitude} : ${place.latLng?.longitude}")
 
                 addMark(place.latLng!!)
+                if (switch == 0) {
+                    fromAddress.text = place.address
+                    fromName.text = place.name
+                } else {
+                    toAddress.text = place.address
+                    toName.text = place.name
+                }
             } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
                 val status = Autocomplete.getStatusFromIntent(data!!)
                 Log.i("AutoComplete", status.statusMessage)
